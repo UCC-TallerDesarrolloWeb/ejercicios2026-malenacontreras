70f7ad55a167784a832abd99c0610c2dd5de61bc
malena <malenacontreras2006@gmail.com>
2026-09-21 08:06:32 -0300

    unidades

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -5,3 +5,13 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function convertirUnidades(id,value){
+    if(isNaN(valor)){//me devuelve si es numerica o no
+        alert("Se ingreso un valor incorrecto: "+id);
+    }else if(id=="metro"){
+        document.getElementById("pulgada").value=valor*39.09361;
+        document.getElementById("pie").value=valor*3.28084;
+        document.getElementById("yarda").value=valor*1.09361;
+
+    }
+}
